554af37a32b0bdcd3efb5d873a7ddcd7c9d7e0d0
unknown <justinnikolai@gmail.com>
2023-03-26 16:29:57 +0800

    Section: Browser exercise

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        var employee = new Employee(
-                50_000,
-                20);
-        int wage = employee.calculateWage(20);
-
-        System.out.println(wage);
-//        var browser = new Browser();
-        Employee.printNumberOfEmployees();
-
+        var browser = new Browser();
 
     }
 }
