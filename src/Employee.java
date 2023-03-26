66e8eb1a8ab10b484d56d2010b38eff5835cfcab
unknown <justinnikolai@gmail.com>
2023-03-26 16:25:33 +0800

    End of Classes Section, Added the ClassesQuiz.txt with answers

--- src/Employee.java
+++ src/Employee.java
@@ -2,13 +2,33 @@ public class Employee {
     private int baseSalary;
     private int hourlyRate;
 
+    //Static members of class
+    public static int numberOfEmployees;
+
     //Constructor
     public Employee(int baseSalary, int hourlyRate){
         setBaseSalary(baseSalary);
         setHourlyRate(hourlyRate);
+        numberOfEmployees++;
+    }
+
+    //Overloading a constructor
+    public Employee(int baseSalary){
+        this(baseSalary, 0);
+    }
+
+    // Static method of class
+    public static void printNumberOfEmployees(){
+        System.out.println(numberOfEmployees);
     }
     public int calculateWage(int extraHours) {
         return baseSalary + (extraHours * hourlyRate);
+    }
+
+    //Overloading a method. with this you can use calculateWage() with extraHours = 0
+    // Overloading is great but do not over use it.
+    public int calculateWage() {
+        return calculateWage(0);
     }
 
     //Getter
@@ -24,7 +44,7 @@ public class Employee {
         }
     }
     private void setHourlyRate(int hourlyRate){
-        if (hourlyRate <= 0){
+        if (hourlyRate < 0){
             throw  new IllegalArgumentException("Hourly rate cannot be less than 1");
         } else {
             this.hourlyRate = hourlyRate;
--- src/Main.java
+++ src/Main.java
@@ -7,6 +7,7 @@ public class Main {
 
         System.out.println(wage);
 //        var browser = new Browser();
+        Employee.printNumberOfEmployees();
 
 
     }
