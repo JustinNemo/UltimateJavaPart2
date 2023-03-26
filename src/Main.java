300694732e7b14528a4f53ab9bed188f96387014
unknown <justinnikolai@gmail.com>
2023-03-26 20:09:46 +0800

    Section: changed the procedural programming to OOP

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,14 @@
 public class Main {
     public static void main(String[] args) {
-        var employee = new Employee(
-                50_000,
-                20);
-        int wage = employee.calculateWage(20);
+            int principal = (int) Console.readNumber("Principal: ", 1000, 1_000_000);
+            float annualInterest = (float) Console.readNumber("Annual Interest Rate: ", 1, 30);
+            byte years = (byte) Console.readNumber("Period (Years): ", 1, 30);
 
-        System.out.println(wage);
-//        var browser = new Browser();
-        Employee.printNumberOfEmployees();
+            var calculator = new MortgageCalculator(principal, annualInterest, years);
+            var report = new MortgageReport(calculator);
 
+            report.printMortgage();
+            report.printPaymentSchedule();
+        }
 
-    }
 }
