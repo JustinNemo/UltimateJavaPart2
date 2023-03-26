e33576e82dcef7271b29989ce24fd1792b7df856
unknown <justinnikolai@gmail.com>
2023-03-26 15:37:43 +0800

    Added constructors in employee class

--- src/Employee.java
+++ src/Employee.java
@@ -2,21 +2,28 @@ public class Employee {
     private int baseSalary;
     private int hourlyRate;
 
+    //Constructor
+    public Employee(int baseSalary, int hourlyRate){
+        setBaseSalary(baseSalary);
+        setHourlyRate(hourlyRate);
+    }
     public int calculateWage(int extraHours) {
         return baseSalary + (extraHours * hourlyRate);
     }
 
+    //Getter
     public int getBaseSalary(){
         return baseSalary;
     }
-    public void setBaseSalary(int baseSalary){
+    //Setter
+    private void setBaseSalary(int baseSalary){
         if (baseSalary <= 0){
             throw new IllegalArgumentException("Base salary cannot be less than 1");
         } else {
             this.baseSalary = baseSalary;
         }
     }
-    public void setHourlyRate(int hourlyRate){
+    private void setHourlyRate(int hourlyRate){
         if (hourlyRate <= 0){
             throw  new IllegalArgumentException("Hourly rate cannot be less than 1");
         } else {
--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,13 @@
 public class Main {
     public static void main(String[] args) {
-//        var employee = new Employee();
-//        employee.setBaseSalary(50_000);
-//        employee.getBaseSalary();
-//        employee.setHourlyRate(20);
-//        employee.getHourlyRate();
-//
-//        int wage = employee.calculateWage(20);
-//
-//        System.out.println(wage);
-        var browser = new Browser();
-        
+        var employee = new Employee(
+                50_000,
+                20);
+        int wage = employee.calculateWage(20);
+
+        System.out.println(wage);
+//        var browser = new Browser();
+
 
     }
 }
